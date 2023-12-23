e5498d2848d657f32a75feb59541dcb7d6490abd
AgastyaPatel <agastya.patel2@gmail.com>
2023-12-23 11:17:17 +0530

    Added code calling import reference from symblink

---
+++ Folder1/main.py
@@ -0,0 +1,11 @@
+from Symblink1 import hello
+print(hello)
+
+hello.hello_world()
+
+
+## OUTPUT
+"""
+<module 'Symblink1.hello' from 'ROOT\\SymblinkGuide\\Folder1\\Symblink1\\hello.py'>
+Hello World, This is script inside of target folder
+"""
